1b090cdb26539f0070219b7c186580f7edf6c240
Oscar Espejo Sagués <xxgreysoulxx@gmail.com>
2019-11-01 22:28:23 +0100

    Adding Card Logic

--- SGBarcelona/Assets/CSVFiles/LoadCartas.cs
+++ SGBarcelona/Assets/CSVFiles/LoadCartas.cs
@@ -4,39 +4,11 @@ using UnityEngine;
 
 public class LoadCartas : MonoBehaviour
 {
-    public List<Card> MyDeck = new List<Card>(); 
 
-    public TextAsset CardsDB;
     // Start is called before the first frame update
     void Start()
     {
-        string[] data = CardsDB.text.Split(new char[] { '\n' }); //separa las linias y las guarda como strings
-        Debug.Log(data.Length);
-
-        for (int i = 1; i<data.Length; i++)
-        {
-            string[] row = data[i].Split(new char[] {','});
-            Card ncard = new Card();
-            //------
-            int.TryParse(row[0], out ncard.Id);
-            ncard.Description = row[1];
-            //------
-            int.TryParse(row[2], out ncard.MoneyY);
-            int.TryParse(row[3], out ncard.HappyY);
-            int.TryParse(row[4], out ncard.CityY);
-            //------
-            int.TryParse(row[5], out ncard.MoneyN);
-            int.TryParse(row[6], out ncard.HappyN);
-            int.TryParse(row[7], out ncard.CityN);
-            //------
-            int.TryParse(row[8], out ncard.SeaLvlY);
-            int.TryParse(row[9], out ncard.SeaLvlN);
-            //------
-            int.TryParse(row[10], out ncard.EventY);
-            int.TryParse(row[11], out ncard.EventN);
-
-            MyDeck.Add(ncard);
-        }
+        
     }
 
     // Update is called once per frame
--- SGBarcelona/Assets/Scripts/StatsGameManager.cs
+++ SGBarcelona/Assets/Scripts/StatsGameManager.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 
 public class StatsGameManager : MonoBehaviour
 {
-
+    public TextAsset CardsDB;
+    public GameObject GameCard;
     public GameObject GameStats;
     public Button ButtonYes;
     public Button ButtonNo;
 
 
+    private List<Card> MyDeck = new List<Card>();
     private Slider _moneyIndicator;
     private Slider _happinessIndicator;
     private Slider _citystateIndicator;
@@ -19,6 +21,7 @@ public class StatsGameManager : MonoBehaviour
 
     void Awake()
     {
+        GenerateDeck();
         _moneyIndicator = GameStats.transform.GetChild(0).GetComponent<Slider>();
         _happinessIndicator = GameStats.transform.GetChild(1).GetComponent<Slider>();
         _citystateIndicator = GameStats.transform.GetChild(2).GetComponent<Slider>();
@@ -48,6 +51,36 @@ public class StatsGameManager : MonoBehaviour
     public void DenyAction()
     {
         Debug.Log("DENY ACTION");
+    }
+
+    public void GenerateDeck()
+    {
+        string[] data = CardsDB.text.Split(new char[] { '\n' }); //separa las linias y las guarda como strings
+        for (int i = 1; i < data.Length; i++)
+        {
+            string[] row = data[i].Split(new char[] { ',' });
+            Card ncard = new Card();
+            //------
+            int.TryParse(row[0], out ncard.Id);
+            ncard.Description = row[1];
+            //------
+            int.TryParse(row[2], out ncard.MoneyY);
+            int.TryParse(row[3], out ncard.HappyY);
+            int.TryParse(row[4], out ncard.CityY);
+            //------
+            int.TryParse(row[5], out ncard.MoneyN);
+            int.TryParse(row[6], out ncard.HappyN);
+            int.TryParse(row[7], out ncard.CityN);
+            //------
+            int.TryParse(row[8], out ncard.SeaLvlY);
+            int.TryParse(row[9], out ncard.SeaLvlN);
+            //------
+            int.TryParse(row[10], out ncard.EventY);
+            int.TryParse(row[11], out ncard.EventN);
+
+            MyDeck.Add(ncard);
+        }
+        Debug.Log("THE NUMBER OF CARDS IS: " + MyDeck.Count);
     }
 
 
