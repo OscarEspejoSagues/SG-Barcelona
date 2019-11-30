35110a633e8c225059872b37b1581ed8e8580525
Oscar Espejo Sagués <xxgreysoulxx@gmail.com>
2019-11-30 16:23:30 +0100

    Main Menu Added + Cards Pulidas

---
+++ SGBarcelona/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene("SceneLvl1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
